6ef5a916d5123505f3c959af1afcb68b6f72ed83
Andrew Johnson <manilda7@gmail.com>
2019-10-25 10:47:13 -0600

    Working through connection object error

--- app/facades/stations_facade.rb
+++ app/facades/stations_facade.rb
@@ -4,11 +4,22 @@ class StationsFacade
   end
 
   def stations
-    conn = Faraday.new("https://developer.nrel.gov") do |f|
+    conn = Faraday.new(url: "https://developer.nrel.gov") do |f|
+      f.params['format'] = ['json']
       f.params['api_key'] = ENV['nrel_api_key']
       f.params['location'] = ["1331 17th St LL100, Denver, CO 80202"]
       f.adapter Faraday.default_adapter
+    end
 
-      binding.pry
+    response = conn.get("/api/alt-fuel-stations/v1/nearest")
+
+    station_search_data = JSON.parse(response.body, symbolize_names: true)
+
+    station_search_data.map do |station_data|
+      Station.new(station_data)
+    end
   end
+
+  private
+  attr_reader :location
 end
